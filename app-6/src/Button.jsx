854b4152b8de63c2aa00c6f473c6f0a65af0c21a
skylar siggard <siggardskylar@gmail.com>
2019-08-29 22:03:07 -0600

    working on 6

---
+++ app-6/src/Button.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export default class Button extends Component {
+  render() {
+      console.log()
+    return (
+      <div className="App">
+        <input type="text" name="" id=""/>
+        <button onClick={() => {} }>Add</button>
+      </div>
+    );
+  }
+}
